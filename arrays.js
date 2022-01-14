5e12d9b2842b43834d2ff1ea0947722c6eeef2af
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2022-01-14 23:46:13 +0000

    Delete arrays.js

--- arrays.js
+++
@@ -1,9 +0,0 @@
-//? CONCAT - JOINING TWO ARRAYS
-
-const array1 = ["Michael", "John", "Mark"];
-const array2 = ["Jeff", "Suzan", "Megan"];
-
-const concat = array1.concat(array2);
-console.log(concat);
-
-//? CONCAT - JOINING THREE ARRAYS
