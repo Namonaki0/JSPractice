f79dcbd7dd78328847ad469b5f292d691c4cdb9f
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2022-01-14 22:34:11 +0000

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,9 @@
+//? CONCAT - JOINING TWO ARRAYS
+
+const array1 = ["Michael", "John", "Mark"];
+const array2 = ["Jeff", "Suzan", "Megan"];
+
+const concat = array1.concat(array2);
+console.log(concat);
+
+//? CONCAT - JOINING THREE ARRAYS
