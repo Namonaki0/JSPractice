39c6f1ec02e64c7b5bd58255761c1cd50489c445
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2022-01-30 22:32:13 +0000

    WINDOW study material

---
+++ WINDOW/HISTORY - forward().js
@@ -0,0 +1,6 @@
+//* <button onclick="history.forward()">go forward</button>;
+
+// -----------------------------------------------
+
+//? The history.forward() method loads the next URL (page) in the history list.
+//? The history.forward() method only works if a next page exists.
