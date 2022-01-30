ceacbe046033a3fe2738f70155f1a7cb6fe268d9
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2022-01-30 23:53:59 +0000

    Delete HISTORY - back().js

--- WINDOW/HISTORY - back().js
+++
@@ -1,6 +0,0 @@
-//* <button onclick="history.back()">go back</button>;
-
-// -----------------------------------------------
-
-//? The history.back() method loads the previous URL (page) in the history list.
-//? The history.back() method only works if a previous page exists.
